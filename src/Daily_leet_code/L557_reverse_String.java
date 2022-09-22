3a2b4082db0f8fdba58bc49a757a55ec9a81e486
Rakesh Chandrapal <rakesh202078@outlook.com>
2022-09-22 08:17:15 +0530

    Leetcode and GFG!!

---
+++ src/Daily_leet_code/L557_reverse_String.java
@@ -0,0 +1,25 @@
+package Daily_leet_code;
+
+import java.util.Arrays;
+
+public class L557_reverse_String {
+    public static void main(String[] args) {
+        String s =  "Let's take LeetCode contest";
+        String ans = reverseWords(s);
+        System.out.println(ans);
+    }
+    public  static String reverseWords(String s) {
+
+        StringBuilder result = new StringBuilder();
+        String []ans = s.split(" ");
+        for(int i = 0; i < ans.length;i++)
+        {
+            StringBuilder newString = new StringBuilder(ans[i]);
+            newString.reverse();
+            result.append(newString);
+            if(i != ans.length - 1)result.append(" ");
+        }
+        return result.toString();
+    }
+
+}
---
+++ src/GFG/Arrays/Daily/IPL2021.java
@@ -0,0 +1,56 @@
+package GFG.Arrays.Daily;
+
+import java.util.ArrayList;
+
+public class IPL2021 {
+
+    public static void main(String[] args) {
+        int [] a= {1 ,2, 3, 1, 4, 5, 2, 3, 6};
+        int k = 3;
+        System.out.println(max_of_subarrays(a,a.length,k));
+    }
+
+    static ArrayList<Integer> max_of_subarrays(int[] arr, int n, int k) {
+        // Your code here
+        int i = 0,j = k - 1;
+        int max = -1;
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int r = 0; r <= j;r++)
+        {
+            max = Math.max(max, arr[r]);
+        }
+        ans.add(max);
+        for(int r = j; r <= arr.length; r++)
+        {
+            max = Math.max(max, arr[r]);
+            ans.add(max);
+        }
+        return ans;
+    }
+    static ArrayList<Integer> max_of_subarrays1(int[] arr, int n, int k) {
+        // Your code here
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int i = 0, j = k - 1;
+
+
+        while (i < arr.length && j < arr.length)
+        {
+          int result = max(arr,i,j);
+          ans.add(result);
+          i++;
+          j++;
+        }
+        return ans;
+    }
+
+    public static int max(int []a, int s,int e)
+    {
+        int max = -1;
+        for(int i = s; i <= e;i++)
+        {
+            max = Math.max(max, a[i]);
+        }
+        return max;
+    }
+}
